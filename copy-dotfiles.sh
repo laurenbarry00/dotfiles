8aa36c382532a9bee652327806369b3f718b51dd
laurenbarry00 <laurenbarry00@gmail.com>
2020-03-26 18:14:33 -0700

    Added terminator config

--- copy-dotfiles.sh
+++ copy-dotfiles.sh
@@ -6,5 +6,6 @@ cp -aTv ~/.config/i3 ./i3/
 cp -aTv ~/.config/polybar ./polybar/
 cp -aTv ~/.config/spotifyd ./spotifyd/
 cp -aTv ~/.config/compton ./compton/
+cp -aTv ~/.config/terminator ./terminator
 cp -aTv ~/.config/sublime-text-3/Packages/THEMES ./sublime-text-3-THEMES
 cp -v /etc/default/tlp ./
