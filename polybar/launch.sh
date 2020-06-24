21b7292a4cc6384fd788ef3974edda9c7e6b6d9b
Lauren <laurenbarry00@gmail.com>
2020-06-24 14:18:48 -0700

    Updated dotfiles

--- polybar/launch.sh
+++ polybar/launch.sh
@@ -5,8 +5,7 @@ killall -q polybar
 # If all your bars have ipc enabled, you can also use 
 # polybar-msg cmd quit
 
-# Launch bar1 and bar2
-echo "---" | tee -a /tmp/polybar.log
+# Launch main bar
 polybar main >>/tmp/polybar.log 2>&1 &
 
 echo "Bars launched..."
